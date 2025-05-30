cbc256d6bf3b8b0590a60ba1782ec7952f9d7f9a
jdharm1966 <dharmonson@trademarkproperty.com>
2025-05-30 15:40:50 -0500

    Create from boxsdk import OAuth2, Client.py

---
+++ content/guides/authentication/oauth2/from boxsdk import OAuth2, Client.py
@@ -0,0 +1,9 @@
+from boxsdk import OAuth2, Client
+
+oauth = OAuth2(
+    client_id='uimujuj7e881pkx6vg35m613shvtf3qw',
+    client_secret='DO3BpWulSrVXtiOKeotdZdT2T7ShaBiM',
+    access_token='4TjV41phUOMagnks4VdJRkwYTTS9n5vA'
+)
+
+client = Client(oauth)
